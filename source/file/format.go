1ebdca8a010a549839b17017157a9299894f6002
samo ratnik <samo.ratnik@gmail.com>
2019-09-22 12:25:32 +0200

    Add v0.14.0

---
+++ source/file/format.go
@@ -0,0 +1,15 @@
+package file
+
+import (
+	"strings"
+
+	"github.com/samotarnik/go-config/encoder"
+)
+
+func format(p string, e encoder.Encoder) string {
+	parts := strings.Split(p, ".")
+	if len(parts) > 1 {
+		return parts[len(parts)-1]
+	}
+	return e.String()
+}
